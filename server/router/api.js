c9d7950af6b825673eec5327819ba19c3f50c04f
John <j0hn4ll3nc4mpb3ll@gmail.com>
2020-11-25 17:48:34 -0500

    bug fixes. proxy server now functioning

---
+++ server/router/api.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const {
+  service1, service2, service3, service4,
+} = require('../config/services.js');
+
+const router = Router();
+
+router.use(service1.api[0], createProxyMiddleware({ target: service1.url, changeOrigin: true }));
+router.use(service1.api[1], createProxyMiddleware({ target: service1.url, changeOrigin: true }));
+router.use(service1.api[2], createProxyMiddleware({ target: service1.url, changeOrigin: true }));
+router.use(service1.api[3], createProxyMiddleware({ target: service1.url, changeOrigin: true }));
+router.use(service2.api[0], createProxyMiddleware({ target: service2.url, changeOrigin: true }));
+router.use(service2.api[1], createProxyMiddleware({ target: service2.url, changeOrigin: true }));
+router.use(service3.api, createProxyMiddleware({ target: service3.url, changeOrigin: true }));
+router.use(service4.api, createProxyMiddleware({ target: service4.url, changeOrigin: true }));
+
+module.exports = router;
